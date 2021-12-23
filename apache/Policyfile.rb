d711fe4f3c753a55b67684993896f6ff0f44bfe3
shikhakhandelwal07 <92728076+shikhakhandelwal07@users.noreply.github.com>
2021-12-23 20:47:36 +0530

    Update Policyfile.rb

--- apache/Policyfile.rb
+++ apache/Policyfile.rb
@@ -4,7 +4,7 @@
 # https://docs.chef.io/policyfile.html
 
 # A name that describes what the system you're building with Chef does.
-name 'coupa_dev-5.0.0'
+name 'apache'
 
 # Where to find external cookbooks:
 default_source :supermarket
